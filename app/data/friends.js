e5fd92e9846a2ed04d251c4e180bc463a9538e8b
5820K <38088640+chuynh18@users.noreply.github.com>
2018-06-01 22:23:08 -0700

    placeholder friends created

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var friends = [
+    {
+        name: "Obi-Wan Kenobi",
+        photo: "placeholder",
+        scores: [1,5,1,5,4,3,3,5,4,1]
+    },
+    {
+        name: "Darth Vader",
+        photo: "placeholder",
+        scores: [5,1,3,2,1,3,3,1,5,5]
+    },
+    {
+        name: "Darth Plagueis",
+        photo: "placeholder",
+        scores: [4,3,2,1,3,1,5,3,4,1]
+    },
+    {
+        name: "Luke Skywalker",
+        photo: "placeholder",
+        scores: [1,1,2,5,4,5,3,4,3,1]
+    },
+    {
+        name: "General Grievous",
+        photo: "placeholder",
+        scores: [1,1,4,2,3,1,4,3,1,1]
+    }
+]
