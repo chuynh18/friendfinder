27f4835e1be7bf60a1d5111e425111347a408c43
5820K <38088640+chuynh18@users.noreply.github.com>
2018-06-02 22:07:41 -0700

    another increment of value.  possible to do the survey.  todo:  client-side validation, styling work, make result appear as a modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,27 +3,32 @@
 exports.friends = [
     {
         name: "Obi-Wan Kenobi",
-        photo: "kenobi-placeholder",
+        photo: "/images/obi.jpg",
         scores: [1,5,1,5,4,3,3,5,4,1]
     },
     {
         name: "Darth Vader",
-        photo: "vader-placeholder",
+        photo: "/images/vader.jpg",
         scores: [5,1,3,2,1,3,3,1,5,5]
     },
     {
         name: "Darth Plagueis",
-        photo: "plagueis-placeholder",
+        photo: "/images/plagueis.png",
         scores: [4,3,2,1,3,1,5,3,4,1]
     },
     {
         name: "Luke Skywalker",
-        photo: "luke-placeholder",
+        photo: "/images/luke.png",
         scores: [1,1,2,5,4,5,3,4,3,1]
     },
     {
         name: "General Grievous",
-        photo: "grevious-placeholder",
+        photo: "/images/grievous.jpg",
         scores: [1,1,4,2,3,1,4,3,1,1]
+    },
+    {
+        name: "The Emperor",
+        photo: "/images/grievous.jpg",
+        scores: [5,5,1,1,3,2,4,3,3,5]
     }
 ]
