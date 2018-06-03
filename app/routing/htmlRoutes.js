4fbd0919b49342ab7205039469314645a6f43a1a
5820K <38088640+chuynh18@users.noreply.github.com>
2018-06-02 19:23:52 -0700

    starting to work on html now snoozers.  added routes for css and js for client-side

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,24 +2,28 @@
 
 var path = require("path");
 
-module.exports = (function() {
+var htmlRoutes = require('express').Router();
 
-    var htmlRoutes = require('express').Router();
+// __dirname provides the absolute path to the directory that contains this file (htmlRoutes.js)
+// so we need to go up one level in order to navigate to our HTML files
+htmlRoutes.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
 
-    // __dirname provides the absolute path to the directory that contains this file (htmlRoutes.js)
-    // so we need to go up one level in order to navigate to our HTML files
-    htmlRoutes.get('/survey', function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
+htmlRoutes.get('/images/:image', function (req, res) {
+    res.sendFile(path.join(__dirname, `../public/images/${req.params.image}`));
+});
 
-    htmlRoutes.get('/images/:image', function (req, res) {
-        var chosen = req.params.image;
-        res.sendFile(path.join(__dirname, `../public/images/${chosen}`));
-    });
-    
-    htmlRoutes.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
+htmlRoutes.get('/css/:css', function (req, res) {
+    res.sendFile(path.join(__dirname, `../public/css/${req.params.css}`));
+});
 
-    return htmlRoutes;
-})();
+htmlRoutes.get('/js/:js', function (req, res) {
+    res.sendFile(path.join(__dirname, `../public/js/${req.params.js}`));
+});
+
+htmlRoutes.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+module.exports = htmlRoutes;
