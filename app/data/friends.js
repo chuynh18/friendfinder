8b72c512a2881fab623e60e72f167984e5976d84
5820K <38088640+chuynh18@users.noreply.github.com>
2018-06-02 18:15:26 -0700

    originally pushed boolean 'false' into the array when receiving invalid value, but now pushing -1 to keep all element types numbers

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,27 +3,27 @@
 exports.friends = [
     {
         name: "Obi-Wan Kenobi",
-        photo: "placeholder",
+        photo: "kenobi-placeholder",
         scores: [1,5,1,5,4,3,3,5,4,1]
     },
     {
         name: "Darth Vader",
-        photo: "placeholder",
+        photo: "vader-placeholder",
         scores: [5,1,3,2,1,3,3,1,5,5]
     },
     {
         name: "Darth Plagueis",
-        photo: "placeholder",
+        photo: "plagueis-placeholder",
         scores: [4,3,2,1,3,1,5,3,4,1]
     },
     {
         name: "Luke Skywalker",
-        photo: "placeholder",
+        photo: "luke-placeholder",
         scores: [1,1,2,5,4,5,3,4,3,1]
     },
     {
         name: "General Grievous",
-        photo: "placeholder",
+        photo: "grevious-placeholder",
         scores: [1,1,4,2,3,1,4,3,1,1]
     }
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,6 @@ var path = require("path");
 var friends = require("../data/friends");
 
 module.exports = (function() {
-
     var apiRoutes = require('express').Router();
 
     // sending a GET request to /api/friends will cause the server to send friends as a JSON
@@ -19,26 +18,25 @@ module.exports = (function() {
     // will cause the server to receive and process the survey results, then respond
     // with the "most compatible" friend
     apiRoutes.post("/friends", (req, res) => {
-
         // ========= LOGIC TO RECEIVE/PARSE CLIENT REQUEST =========
-        var surveyResult = [];
+        var surveyResult = []; // will contain the parseInt-ed survey results coming from the user
         var rejected = false;
         Object.keys(req.body).forEach(element => {
             var tempAnswer = parseInt(req.body[element]);
             if (tempAnswer < 1) { // reject numbers smaller than 1
-                surveyResult.push(false);
+                surveyResult.push(-1);
                 if (!rejected) {
                     rejected = true;
                 }
             }
             else if (tempAnswer > 5) { // reject numbers larger than 5
-                surveyResult.push(false);
+                surveyResult.push(-1);
                 if (!rejected) {
                     rejected = true;
                 }
             }
             else if (isNaN(tempAnswer)) { // reject non-numbers
-                surveyResult.push(false);
+                surveyResult.push(-1);
                 if (!rejected) {
                     rejected = true;
                 }
@@ -47,8 +45,6 @@ module.exports = (function() {
                 surveyResult.push(tempAnswer);
             }
         });
-        
-
         if (surveyResult.length > 10) { // reject requests with more than 10 keys
             res.send("Error:  request has too many keys.  Expected 10 keys.");
             if (!rejected) {
@@ -61,19 +57,15 @@ module.exports = (function() {
                 rejected = true;
             }
         }
-        else if (surveyResult.indexOf(false) > -1) { // reject requests that contain invalid values (examine the forEach loop above)
+        else if (surveyResult.indexOf(-1) > -1) { // reject requests that contain invalid values (see forEach loop above)
             res.send("Error:  request contains invalid value.");
         }
-        // this block to be deleted after code below is complete
-        else if (surveyResult.length === 10) { // accept valid requests
-            res.send(surveyResult);
-        }
+
         if (rejected) {
             console.log("\nInvalid request received.  Rejecting request...");
         }
         else {
             console.log("\nValid request received.")
-
             // ========= LOGIC TO PROCESS THE REQUEST AND FIND CLOSEST MATCH =========
             var friendsCopy = [...friends.friends];
             var currentMatch = {name: "this homework is DARTH TEDIOUS", difference: 420};
@@ -85,7 +77,7 @@ module.exports = (function() {
                 // iterate through and compare every single score
                 for (var j = 0; j < friendsCopy[i].scores.length; j++) {
                     var diff = surveyResult[j] - friendsCopy[i].scores[j];
-                    // if the difference is negative, multiply by -1 (same result as absolute value)
+                    // if diff is negative, multiply it by -1 and store that value (same result as absolute value)
                     if (diff < 0) {
                         diff = (-1) * diff;
                     }
@@ -96,13 +88,13 @@ module.exports = (function() {
                 if (currentMatch.difference > totalDiff) {
                     currentMatch.difference = totalDiff;
                     currentMatch.name = friendsCopy[i].name;
+                    currentMatch.photo = friendsCopy[i].photo;
                 }
             }
             console.log(`match found: ${JSON.stringify(currentMatch)}`);
         // ========= LOGIC TO SEND BACK CLOSEST MATCH TO CLIENT =========
-
+            res.send(currentMatch);
         }
     })
-
     return apiRoutes;
 })();
