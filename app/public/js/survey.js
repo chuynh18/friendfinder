530e9b885ea1fe6f55ad995449c7799642d76b3c
Acer <38088640+chuynh18@users.noreply.github.com>
2018-06-03 20:17:12 -0700

    working XHR POST and receive response.  fully working client-side validation.  need modals

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,18 +1,61 @@
 "use strict";
 
 var validForm = false;
+var buildUnanswered;
+var indexOfUnchecked;
+var spliceUnchecked;
+var submitClicked = false;
+
 // client-side validation for the convenience of the user (I also have server-side validation, of course)
 // this function will be run when submit is clicked.  it will let the user know if they have unanswered questions
 var onSubmit = function() {
-    var unchecked = [];
+    var formData = "";
+    var unchecked = []; // array of unanswered questions so we can inform the user
+    var surveyData = document.getElementById("charQuiz");
+
+    buildUnanswered = function() {
+        var unanswered = "";
+    
+        if (unchecked.length > 0) {
+            // build out list of unanswered questions so we can show it to the user
+            for (var i = 0; i < unchecked.length; i++) {
+                if (i < unchecked.length-2) {
+                    unanswered += unchecked[i] + ", ";
+                }
+                else if (i < unchecked.length-1) {
+                    unanswered += unchecked[i] + " and ";
+                }
+                else {
+                    unanswered += unchecked[i];
+                }
+            }
+            // tell user which questions have not been answered
+            document.getElementById("warning").textContent = "You didn't answer the following question(s):  " + unanswered + ".  Go back and answer them, then resubmit the form.";
+        }
+        else {
+            document.getElementById("warning").innerHTML = "<br>";
+        }
+        
+    }
+
+    indexOfUnchecked = function(int) {
+        return unchecked.indexOf(int);
+    }
+
+    spliceUnchecked = function(index) {
+        unchecked.splice(index, 1);
+    }
+
     for (var i = 1; i < 11; i++) { // the 11 is hardcoded because I know I wrote 10 questions
         var elements = document.getElementsByClassName("q"+i);
         var checked = false;
         for (var j = 0; j < elements.length; j++) {
             if (elements[j].checked) {
                 checked = true;
+                formData += `q${i}=${j+1}&`
             }
         }
+        // builds list of unanswered questions
         if (!checked) {
             unchecked.push(i);
             // make the unanswered question red
@@ -27,37 +70,47 @@ var onSubmit = function() {
         }
     }
     if (unchecked.length > 0) {
-        var unanswered = "";
-        for (var i = 0; i < unchecked.length; i++) {
-            if (i < unchecked.length-2) {
-                unanswered += unchecked[i] + ", ";
-            }
-            else if (i < unchecked.length-1) {
-                unanswered += unchecked[i] + " ";
-            }
-            else {
-                unanswered += "and " + unchecked[i];
-            }
-        }
-        // tell user which questions have not been answered
-        document.getElementById("warning").textContent = "You didn't answer the following questions:  " + unanswered + ".  Go back and answer them, then resubmit the form.";
+        buildUnanswered();
+        submitClicked = true;
     }
     else {
         validForm = true;
         document.getElementById("warning").textContent = "";
+
+        formData = formData.slice(0,formData.length-1);
+        console.log(formData);
+        // send data to server
+        var xhr = new XMLHttpRequest(); 
+
+        xhr.onreadystatechange = function() {
+            console.log(xhr.response);
+        }
+
+        xhr.open("POST", "/api/friends", true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send(formData);
     }
 }
 
+// attach event listeners to radio buttons that will remove red as appropriate
+// user will only see this if they epic fail the first time around
 for (var i = 0; i < 11; i++) { // once again, the 11 is hardcoded because I know I wrote 10 questions
     var radio = document.getElementsByClassName("q"+i);
     for (var j = 0; j < radio.length; j++) {
         // add event listeners to all the radio buttons
         radio[j].addEventListener("click", function(event) {
-            var relatedRadio = document.getElementsByClassName(this.classList[0]);
-            for (var k = 0; k < relatedRadio.length; k++) {
-                if (document.getElementById(this.classList[0]).classList.contains("warning")) {
-                    document.getElementById(this.classList[0]).classList.remove("warning");
+            if (document.getElementById(this.classList[0]).classList.contains("warning")) {
+                document.getElementById(this.classList[0]).classList.remove("warning");
+            }
+            
+            // removes question from the list of red
+            var questionNumber = parseInt(this.classList[0].slice(1,this.classList[0].length));
+            if (submitClicked) {
+                var index = indexOfUnchecked(questionNumber);
+                if (index > -1) {
+                    spliceUnchecked(index);
                 }
+                buildUnanswered();
             }
         })
     }
@@ -65,7 +118,4 @@ for (var i = 0; i < 11; i++) { // once again, the 11 is hardcoded because I know
 
 document.getElementById("submitButton").addEventListener("click", event => {
     onSubmit();
-    if (!validForm) {
-        event.preventDefault();
-    }
 })
